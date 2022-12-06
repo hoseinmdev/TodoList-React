f545fb8cc660a640e1b1f795cc1b312347889f41
“HoseinMahmoudi” <Hoseindev@gmail.com>
2022-12-06 23:03:27 +0330

    feat : input component added

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -1,9 +1,21 @@
+import { useState, useRef } from "react";
+import styles from "./todoInput.module.css";
+
 const TodoInput = () => {
+  const [value, setValue] = useState("");
+  const inputBox = useRef();
+
+  const inputHandler = () => {
+    const value = inputBox.current.value;
+    setValue(value);
+    console.log(value)
+  };
+
   return (
     <div>
-      <div>
-        <input />
-        <button>افزودن</button>
+      <div className={styles.todoInput}>
+        <input ref={inputBox} placeholder="عنوان را وارد کنید" />
+        <button onClick={inputHandler}>افزودن</button>
       </div>
     </div>
   );
